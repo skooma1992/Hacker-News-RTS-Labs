effff18208e08fa2eb449c5be2e6fc3f8c85f2a9
skooma1992 <upsonjake@gmail.com>
2020-08-10 20:32:40 -0400

    Updates

---
+++ precache-manifest.28b1a4aed7f837fa165c31224552e4c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0a3f0f8d6601c8c64a9",
+    "url": "/Hacker-News-RTS-Labs/static/css/main.fc41cd47.chunk.css"
+  },
+  {
+    "revision": "c0a3f0f8d6601c8c64a9",
+    "url": "/Hacker-News-RTS-Labs/static/js/main.c0a3f0f8.chunk.js"
+  },
+  {
+    "revision": "64d0fb3e2356f5a6f977",
+    "url": "/Hacker-News-RTS-Labs/static/js/runtime~main.64d0fb3e.js"
+  },
+  {
+    "revision": "5abe0fd5253f65dee984",
+    "url": "/Hacker-News-RTS-Labs/static/js/2.5abe0fd5.chunk.js"
+  },
+  {
+    "revision": "ead987031f24adef2b0c9b20fef714c1",
+    "url": "/Hacker-News-RTS-Labs/index.html"
+  }
+];
